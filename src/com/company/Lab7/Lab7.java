df6060078260544073312385fb4a2e47dfa44c5e
Arseniy <arsanav@gmail.com>
2022-04-11 12:34:12 +0300

    7 laba, part 7.1

---
+++ src/com/company/Lab7/Lab7.java
@@ -0,0 +1,50 @@
+package com.company.Lab7;
+
+import java.util.Locale;
+
+public class Lab7 {
+
+//    private static final String text =  "В русском тексте каждую букву заменить ее порядковым номером в \n" +
+//            "алфавите. При выводе в одной строке печатать текст с двумя\n" +
+//            "пробелами между буквами, в следующей строке внизу\n" +
+//            "под каждой буквой печатать ее номер.";
+//    private static final String alphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+//
+//    private static void printSymbolLine(String line) {
+//        for (char symbol : line.toCharArray()) {
+//            System.out.print(" " + symbol + " ");
+//        }
+//        System.out.println();
+//    }
+//
+//    private static void printNumberLine(String line) {
+//        int number;
+//        for (char sym : line.toCharArray()) {
+//            number = alphabet.indexOf(sym);
+//            if (number == -1){
+//                number = alphabet.toUpperCase(Locale.ROOT).indexOf(sym);
+//                if (number == -1){
+//                    System.out.print("-- ");
+//                    continue;
+//                }
+//            }
+//            System.out.printf("%2d ",(number + 1));
+//        }
+//        System.out.println();
+//    }
+//
+//    public static void main(String[] args){
+//        String[] lines = text.split("\\n");
+//        for (String line : lines) {
+//            line = line.replaceAll("\\s+", "");
+//            printSymbolLine(line);
+//            printNumberLine(line);
+//        }
+//
+//    }
+
+
+    public static void main(String[] args){
+
+    }
+}
