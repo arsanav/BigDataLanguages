c366c550b14a341613c2792b636199550edf1b06
Arseniy <arsanav@gmail.com>
2022-04-11 21:14:16 +0300

    7 laba, part 7.4

--- src/com/company/Lab7/Lab7.java
+++ src/com/company/Lab7/Lab7.java
@@ -1,6 +1,8 @@
 package com.company.Lab7;
 
-import java.util.Locale;
+import com.sun.source.tree.Tree;
+
+import java.util.*;
 
 public class Lab7 {
 
@@ -44,7 +46,67 @@ public class Lab7 {
 //    }
 
 
-    public static void main(String[] args){
+//    public static void main(String[] args){
+//    String text =  "В русском тексте каждую букву заменить ее порядковым номерам в \n" +
+//            "алфавите. При выводе в одной строке печатать текст с двумя\n" +
+//            "прабелами между буквами, в следующей страке внизу\n" +
+//            "под каждой буквой печатать ее номер.";
+//    System.out.println(text.replaceAll("ра", "ро"));
+//    }
 
+
+//    public static void main(String[] args){
+//        String text =  "В русском тексте каждую букву заменить ее порядковым номерам в \n" +
+//            "алфавите. При выводе в одной строке печатать текст с двумя \n" +
+//            "прабелами между буквами, в следующей страке внизу \n" +
+//            "под каждой буквой печатать ее номер.";
+//
+//        String [] words = text.toLowerCase().replaceAll("[.?!,\n]", "").split(" ");
+//        TreeMap<String, Integer> wordsTreeMap = new TreeMap<>();
+//        for (String word : words){
+//            Integer number = wordsTreeMap.get(word);
+//            if (number == null){
+//                number = 0;
+//            }
+//                number++;
+//                wordsTreeMap.put(word, number);
+//        }
+//        for (String word : wordsTreeMap.keySet()){
+//            System.out.println(word + ": " + wordsTreeMap.get(word));
+//        }
+//
+//    }
+
+
+    public static void main(String[] args){
+        String text = """
+                В русском тексте каждую букву заменить ее порядковым номерам в\s
+                алфавите. При выводе в одной строке печатать текст с двумя\s
+                прабелами между буквами, в следующей страке внизу\s
+                под каждой буквой печатать ее номер.""";
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите количество символов: ");
+        if (scanner.hasNextInt()) {
+            n = scanner.nextInt();
+        } else {
+            System.out.println("error");
+            n = -1;
+        }
+
+        char[] symbols = text.toLowerCase().replaceAll("[\n\\s]", "").toCharArray();
+        HashMap<Character, Integer> symbolsHashMap = new HashMap<>();
+        for (char sym : symbols){
+            Integer number = symbolsHashMap.get(sym);
+            if (number == null){
+                number = 0;
+            }
+                number++;
+                symbolsHashMap.put(sym, number);
+        }
+        symbolsHashMap.entrySet().stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .limit(n).forEach(System.out::println);
     }
+
 }
